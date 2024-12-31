506584c3ea0f79fccd872d1afd7ff9c0e061a05f
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-01-01 03:22:57 +0530

    integrated routes in app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
 import healthRoute from './routes/healthRoute.js';
 import cors from 'cors';
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.use('/auth', authRoutes);
+app.use('/tasks', taskRoutes);
 app.use('/health', healthRoute);
 
 // Error handling middleware

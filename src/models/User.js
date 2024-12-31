eb35e66f3dede6c7642230b294342c5b0defa228
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-01-01 03:16:38 +0530

    created pre save function in user schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -26,5 +27,12 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 });
 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 export const User = mongoose.model('User', userSchema);
